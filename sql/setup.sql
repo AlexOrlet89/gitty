a097548023886f8286b8f3c0fc2679198f732829
Alex Orlet <AlexOrlet89@gmail.com>
2022-07-01 12:09:29 -0700

    db table set up, app routes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS geets;
+
+-- it's uhh 
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   username VARCHAR NOT NULL,
   email VARCHAR
+);
+CREATE TABLE geets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT ,
+  geet VARCHAR
 );
